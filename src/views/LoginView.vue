8925b22d00043eeb92227e4826e2d07a48b4598a
Andrés notami <andresnotami@gmail.com>
2022-03-17 20:59:07 -0500

    💥 NEW: Design Login

---
+++ src/views/LoginView.vue
@@ -0,0 +1,55 @@
+<template>
+    <section class="container">
+        <div class="cards">
+            <form class="form-control">
+                <h2>Login</h2>
+                <label for="username">User Name</label>
+                <input placeholder="User" required name="login[username]" type="text" />
+
+                <label class="custom-mt" for="password">Password</label>
+                <input
+                    class="password"
+                    placeholder="Password"
+                    required
+                    name="login[password]"
+                    type="password"
+                />
+                <div class="input-groups">
+                    <div class="input-group">
+                        <input type="submit" value="Login" />
+                        &nbsp;
+                        &nbsp;
+                        <button
+                            @click="register()"
+                        >Register</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data() {
+        return {
+
+        }
+    },
+    mounted() {
+
+    },
+    methods: {
+        register() {
+            this.$router.push({
+                path: '/register'
+            })
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/form.scss";
+</style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,62 @@
+<template>
+    <section class="container">
+        <div class="cards">
+            <form class="form-control">
+                <h2>Register</h2>
+                <label for="name">Name</label>
+                <input
+                    class="demo-text"
+                    placeholder="User"
+                    required
+                    name="register[name]"
+                    type="text"
+                />
+
+                <label class="custom-mt" for="username">UserName</label>
+                <input
+                    class="demo-text"
+                    placeholder="UserName"
+                    required
+                    name="register[username]"
+                    type="text"
+                />
+
+                <label class="custom-mt" for="email">Email</label>
+                <input
+                    class="demo-text"
+                    placeholder="Mail"
+                    required
+                    name="register[email]"
+                    type="email"
+                />
+
+                <label class="custom-mt" for="password">Password</label>
+                <input
+                    class="password"
+                    placeholder="Password"
+                    required
+                    name="register[password]"
+                    type="password"
+                />
+                <div class="input-groups">
+                    <div class="input-group">
+                        <input type="submit" value="Save" />
+                        &nbsp;
+                        &nbsp;
+                        <router-link to="/login">Back Login</router-link>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "register"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/form.scss";
+</style>
