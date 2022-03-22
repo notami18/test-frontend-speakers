b9d10654b8d447a7aca5c610e85bf49b3aff1d86
Andrés notami <andresnotami@gmail.com>
2022-03-21 23:01:07 -0500

    💥 NEW: Dashboard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,117 @@
 <template>
-  <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <ul class="bar">
-      <li>Hoaoioa oajpoa </li>
-      <li>Hoaoioa oajpoa </li>
-      <li>Hoaoioa oajpoa </li>
-    </ul>
-  </div>
+  <section class="container">
+    <div class="bar-horizontal">
+      <div class="title-dashboard">
+        <span>Dashboard</span>
+      </div>
+      <span class="avatar"></span>
+      <span class="user-name">Carlos Andres</span>
+    </div>
+    <div class="bar-vertical">
+      <h1 class="title-bar">System</h1>
+      <nav>
+        <ul class="mainMenu">
+          <li>
+            <a href="#">
+              <i class="fa-solid fa-user-group"></i> &nbsp;&nbsp; Users
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <button class="add-users">
+      <i class="fa-solid fa-plus" style="color: #ffffff"></i>
+      &nbsp;&nbsp;
+      <span class="text-users-button">Add new users</span>
+    </button>
+    <div class="table-users">
+      <div class="table">
+        <el-table :data="tableData" stripe style="width: 100%">
+          <el-table-column v-for="column in tableColumns" :key="column.label" v-bind="column"></el-table-column>
+          <el-table-column min-width="100px" align="center" label="Options">
+            <div slot-scope="{ row }" class="d-flex">
+              <a @click.prevent="onDetails(row)">View todos</a>
+            </div>
+          </el-table-column>
+        </el-table>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { Table, TableColumn } from 'element-ui';
+import userServices from '../services/user'
 export default {
   name: 'HomeView',
-  // components: {
-  //   HelloWorld
-  // }
+  components: {
+    [Table.name]: Table,
+    [TableColumn.name]: TableColumn
+  },
+  data() {
+    return {
+      tableColumns: [
+        {
+          prop: 'name',
+          label: 'Name',
+          minWidth: 160,
+          sortable: true
+        },
+        {
+          prop: 'username',
+          label: 'UserName',
+          minWidth: 220,
+          sortable: true
+        },
+        {
+          prop: 'email',
+          label: 'Email',
+          minWidth: 135,
+          sortable: true
+        },
+        {
+          prop: 'address',
+          label: 'Address',
+          minWidth: 100,
+          sortable: true
+        },
+        {
+          prop: 'company',
+          label: 'Company',
+          minWidth: 150,
+          sortable: true
+        }
+      ],
+      tableData: [],
+    }
+  },
+  mounted() {
+    this.getUsers()
+    console.log(this.dataUsers)
+  },
+
+  methods: {
+    async getUsers() {
+      const { data } = await userServices.user()
+      console.log(data)
+      this.tableData = data.map(user => {
+        return {
+          id: user.id,
+          name: user.name,
+          username: user.username,
+          email: user.email,
+          address: `${user.address.city}, ${user.address.street}, ${user.address.suite}`,
+          company: user.company.name
+        }
+      })
+    },
+    onDetails(row) {
+      console.log(row)
+    }
+  },
 }
 </script>
 <style lang="scss">
-.bar {
-  position: absolute;
-  width: 245px;
-  height: 716px;
-  left: -3px;
-  top: 0px;
-
-  background: #ffffff;
-  box-shadow: 4px 0px 4px rgba(43, 45, 55, 0.01);
-}
+@import "../assets/style/index.scss";
+@import url("//unpkg.com/element-ui@2.15.7/lib/theme-chalk/index.css");
 </style>
